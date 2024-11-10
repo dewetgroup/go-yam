1d870d979070894300a41c23710a6c9f1fa3813d
ndewet <55186106+ndewet@users.noreply.github.com>
2024-11-10 16:30:57 +0100

    Add LOCK, and UNLOCK, methods

--- method.go
+++ method.go
@@ -2,6 +2,9 @@ package yam
 
 type Method string
 
+// Should you require a method not listed here,
+// you can simply provide the string value when routing.
+
 const (
 	GET     Method = "GET"
 	POST    Method = "POST"
@@ -12,4 +15,6 @@ const (
 	OPTIONS Method = "OPTIONS"
 	CONNECT Method = "CONNECT"
 	TRACE   Method = "TRACE"
+	LOCK    Method = "LOCK"
+	UNLOCK  Method = "UNLOCK"
 )
